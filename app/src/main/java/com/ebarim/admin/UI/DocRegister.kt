823b36523d2cdb85d6dcc8c234f04fb469c91e0f
NihalP01 <Nihalburagohain02@gmail.com>
2021-09-02 23:41:14 +0530

    added workable register button

--- app/src/main/java/com/ebarim/admin/UI/DocRegister.kt
+++ app/src/main/java/com/ebarim/admin/UI/DocRegister.kt
@@ -3,6 +3,7 @@ package com.ebarim.admin.UI
 import android.content.Intent
 import android.os.Bundle
 import android.util.Patterns
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.ebarim.admin.Network.API.ApiClient
 import com.ebarim.admin.Utils.Utils.Companion.AUTH_TYPE
@@ -11,12 +12,13 @@ import com.ebarim.admin.Utils.Utils.Companion.getAuthType
 import com.ebarim.admin.Utils.Utils.Companion.getToken
 import com.ebarim.admin.Utils.Utils.Companion.toast
 import com.ebarim.admin.databinding.ActivityDocRegisterBinding
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 
-class DocRegister: AppCompatActivity() {
-    lateinit var binding:ActivityDocRegisterBinding
+class DocRegister : AppCompatActivity() {
+    lateinit var binding: ActivityDocRegisterBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
@@ -30,58 +32,90 @@ class DocRegister: AppCompatActivity() {
         setContentView(view)
     }
 
-    private fun doValidation(){
-        if (binding.userName.text.toString().isEmpty() || binding.userName.text.toString().length < 5){
+    private fun doValidation() {
+        if (binding.userName.text.toString()
+                .isEmpty() || binding.userName.text.toString().length < 5
+        ) {
             binding.userName.error = "Enter a valid name"
             binding.userName.requestFocus()
             return
         }
-        if (binding.userPhoneNumber.text.toString().isEmpty() || !Patterns.PHONE.matcher(binding.userPhoneNumber.text.toString()).matches()){
+        if (binding.userPhoneNumber.text.toString()
+                .isEmpty() || !Patterns.PHONE.matcher(binding.userPhoneNumber.text.toString())
+                .matches()
+        ) {
             binding.userPhoneNumber.error = "Enter a valid phone number"
             binding.userPhoneNumber.requestFocus()
             return
         }
-        if (binding.userSignupEmail.text.toString().isEmpty() || !Patterns.EMAIL_ADDRESS.matcher(binding.userSignupEmail.text.toString()).matches()){
+        if (binding.userSignupEmail.text.toString().isEmpty() || !Patterns.EMAIL_ADDRESS.matcher(
+                binding.userSignupEmail.text.toString()
+            ).matches()
+        ) {
             binding.userSignupEmail.error = "Please enter a valid email"
             binding.userSignupEmail.requestFocus()
             return
         }
-        if (binding.userSignupPass.text.toString().isEmpty()){
+        if (binding.userSignupPass.text.toString().isEmpty()) {
             binding.userSignupPass.error = "Please enter you password"
             binding.userSignupPass.requestFocus()
             return
         }
-        if (binding.userConfirmPass.text.toString().isEmpty() || binding.userSignupPass.text.toString() != binding.userConfirmPass.text.toString()){
+        if (binding.userConfirmPass.text.toString()
+                .isEmpty() || binding.userSignupPass.text.toString() != binding.userConfirmPass.text.toString()
+        ) {
             binding.userConfirmPass.error = "Password did not match"
             binding.userConfirmPass.requestFocus()
             return
         }
-        docRegister()
+        showAlert()
     }
 
-    private fun docRegister(){
+    private fun docRegister() {
         GlobalScope.launch(Dispatchers.Main) {
             try {
                 val type = getAuthType(AUTH_TYPE)
                 val token = getToken(TOKEN_KEY)
-                val response = ApiClient.ApiAdapter.apiClient.register("$type $token",
+                binding.btnDocRegister.visibility = View.INVISIBLE
+                binding.signupProgressBar.visibility = View.VISIBLE
+                val response = ApiClient.ApiAdapter.apiClient.register(
+                    "$type $token",
                     binding.userName.text.toString(),
                     binding.userPhoneNumber.text.toString(),
                     binding.userSignupEmail.text.toString(),
                     binding.userSignupPass.text.toString()
                 )
-                if (response.isSuccessful && response.body() != null){
+                if (response.isSuccessful && response.body() != null) {
                     //show success message
+                    binding.btnDocRegister.visibility = View.VISIBLE
+                    binding.signupProgressBar.visibility = View.INVISIBLE
                     toast("Registered Successfully")
                     startActivity(Intent(this@DocRegister, Home::class.java))
                     finish()
-                }else{
+                } else {
                     //show error
-                    toast("Logged in failed")
+                    binding.btnDocRegister.visibility = View.VISIBLE
+                    binding.signupProgressBar.visibility = View.INVISIBLE
+                    toast("Failed to create an account.")
                 }
-            }catch (e: Exception){
+            } catch (e: Exception) {
+                binding.btnDocRegister.visibility = View.VISIBLE
+                binding.signupProgressBar.visibility = View.INVISIBLE
                 toast("Something went wrong, try again later.")
             }
         }
+    }
+
+    private fun showAlert() {
+        MaterialAlertDialogBuilder(this)
+            .setTitle("Do you want to continue?")
+            .setMessage("By confirming, a new doctor account will be created.")
+            .setNegativeButton("Cancel") { dialog, which ->
+                //
+            }
+            .setPositiveButton("Confirm") { dialog, which ->
+                docRegister()
+            }
+            .show()
     }
 }
--- app/src/main/java/com/ebarim/admin/UI/Home.kt
+++ app/src/main/java/com/ebarim/admin/UI/Home.kt
@@ -3,6 +3,8 @@ package com.ebarim.admin.UI
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.ebarim.admin.Utils.Utils.Companion.USER_NAME
+import com.ebarim.admin.Utils.Utils.Companion.getName
 import com.ebarim.admin.databinding.ActivityHomeBinding
 
 class Home : AppCompatActivity() {
@@ -15,10 +17,8 @@ class Home : AppCompatActivity() {
         binding.docRegister.setOnClickListener {
             startActivity(Intent(this, DocRegister::class.java))
         }
-
-        binding.showDoctor.setOnClickListener {
-            startActivity(Intent(this, DoctorsList::class.java))
-        }
+        val userName = getName(USER_NAME)
+        binding.userMsg.text = "Hello ${userName}. Press the below button to register a doctor !"
 
         val view = binding.root
         setContentView(view)
